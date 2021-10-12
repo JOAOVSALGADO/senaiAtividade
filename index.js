5e40eded99c501909608457fc1b26961881aac26
JOAOVSALGADO <91294776+JOAOVSALGADO@users.noreply.github.com>
2021-10-12 20:11:49 -0300

    exercicio1aula8

---
+++ index.js
@@ -0,0 +1,19 @@
+var input = (10)
+
+var idade = input
+var quantidade = 0
+
+for (quantidade = 0; quantidade <=102;quantidade++)
+
+if (quantidade<=100)
+{console.log("É permitido cadastro",quantidade)}
+
+
+else if (quantidade>100)
+
+console.log("Não é permitido cadastro!!!");
+
+else (idade < 18);
+
+
+
