45af2f942b92e26e656e488c48f26a4c9fdc3c9d
JOAOVSALGADO <91294776+JOAOVSALGADO@users.noreply.github.com>
2021-10-14 09:23:20 -0300

    dataidadequantidade

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-var input = (10)
+//CADASTRO DE DATA
+var d = new Date();
 
-var idade = input
+console.log(d.getDate())   
+
+if (d = d)
+{console.log("Data disponivel, gentileza seguir com o cadastro!!!")}
+
+else
+
+console.log ("Não é permitido o cadastro");
+
+//IDADE
+
+var idade = 18
+
+if (idade >=18)
+
+console.log("Você é maior que de idade, gentileza seguir com o cadastro!!!");
+
+else
+console.log("não é permitido cadastro!");
+
+//IDADE
 var quantidade = 0
 
 for (quantidade = 0; quantidade <=102;quantidade++)
@@ -13,7 +34,23 @@ else if (quantidade>100)
 
 console.log("Não é permitido cadastro!!!");
 
-else (idade < 18);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
